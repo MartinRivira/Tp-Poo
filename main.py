37a4c11a87f409bd8313711a5d065e4d2941df26
Martin <riviramartin2@gmail.com>
2023-10-23 20:37:56 -0300

    terminando

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-#from controller.cart_controller import CartController
-#from models.product import Product
-
 from controller.cart_controller import CartController
 from models.product import Product
 from models.cart import Cart
@@ -9,14 +6,17 @@ from models.supermarket import Supermarket
 
 
 if __name__ == "__main__":
-    leche = Product("leche", 2.0)
-    sandia = Product("sandia", 6.0)
-    oreo = Product("oreo", 1.0)
+    leche = Product("leche", 340)
+    sandia = Product("sandia", 1174)
+    oreo = Product("oreo", 423)
+    
 
     cart_controller = CartController()
     cart_controller.add_product(leche)
     cart_controller.add_product(sandia)
     cart_controller.add_product(oreo)
+    
+
 
     total_cost = cart_controller.calculate_total_cost()
 
--- models/Discount_Handler.py
+++ models/Discount_Handler.py
@@ -7,18 +7,18 @@ class DiscountHandler:
         total_cost = cart.calculate_total_cost()
         if self.next_handler is not None:
             discounted_cost = self.next_handler.apply_discount(cart)
-            # Aplicar descuento adicional según el manejador actual
+            
             return discounted_cost - (self.get_discount() * total_cost)
         return total_cost - (self.discount * total_cost)
 
     def get_discount(self):
         return self.discount
 
-# Luego, puedes crear subclases de DiscountHandler con descuentos específicos
+
 class LecheDiscountHandler(DiscountHandler):
     def __init__(self):
-        super().__init__(0.1)  # 10% de descuento
+        super().__init__(5)  
 
 class SandiaDiscountHandler(DiscountHandler):
     def __init__(self):
-        super().__init__(0.05)  # 5% de descuento
+        super().__init__(0.5)  
