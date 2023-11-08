6787bee433d8854e5290d4a41a32679bd127c1cc
Martin <riviramartin2@gmail.com>
2023-11-08 20:42:21 -0300

    arreglando

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 from controller.cart_controller import CartController
-from models.product import Product
-from models.Discount_Handler import LecheDiscountHandler, SandiaDiscountHandler, OreoDiscountHandler
+from domain.product import Product
+from domain.Discount_Handler import LecheDiscountHandler, SandiaDiscountHandler, OreoDiscountHandler
+from service.Cart_Service import CartService
 
 if __name__ == "__main__":
     leche = Product("leche", 340)
     sandia = Product("sandia", 1174)
     oreo = Product("oreo", 423)
-    panintegral = Product("paninegral", 500)
+    panintegral = Product("panintegral", 500)
 
-    cart_controller = CartController()
-    cart_controller.add_product(leche)
-    cart_controller.add_product(sandia)
-    cart_controller.add_product(oreo)
-    cart_controller.add_product(panintegral)
+    cart_service = CartService()
+    cart_service.add_product(leche)
+    cart_service.add_product(sandia)
+    cart_service.add_product(oreo)
+    cart_service.add_product(panintegral)
 
-    
     leche_discount = LecheDiscountHandler()
     sandia_discount = SandiaDiscountHandler()
     oreo_discount = OreoDiscountHandler()
 
     leche_discount.next_handler = sandia_discount
-    cart_controller.apply_discount_handler(leche_discount)
+    cart_service.apply_discount(leche_discount)
 
-    total_cost = cart_controller.calculate_total_cost()
+    total_cost = cart_service.calculate_total_cost()
 
     print(f"Total cost: ${total_cost}")
--- service/Cart_Service.py
+++ service/Cart_Service.py
@@ -1,11 +1,15 @@
-from models.cart import Cart
+from domain.cart import Cart
 
 class CartService:
     def __init__(self):
         self.cart = Cart()
 
-    def add_item(self, product):
+    def add_product(self, product):
         self.cart.add_item(product)
 
     def calculate_total_cost(self):
         return self.cart.calculate_total_cost()
+
+    def apply_discount(self, discount_handler):
+        
+        pass
